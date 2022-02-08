01e5c8f17d94e0a3f2d522338926c357322899ad
pacoguerraq <franciscoguerraquintanilla@gmail.com>
2022-02-08 03:29:56 -0600

    index acabado

--- js/jornadas.js
+++ js/jornadas.js
@@ -16,40 +16,40 @@ function mostrarNombres(numTarjeta, IDlista1, IDlista2, IDcamisa1, IDcamisa2) {
 	
 	
 	/* Cerounos */
-	let nombresFuenteOvejuna = ['Chema Guerrero', 'Eduardo Quintanar', 'Oscar Vela', 'Angel Celis', 'Diego González', 'Angel Salinas', 'Pablo García', 'Jaime Farias', 'Roberto Casanova', 'Luis Franco', 'Miguel Camacho'];
+	let nombresFuenteOvejuna = ['Gene ??', 'Chema Guerrero', 'Eduardo Quintanar', 'Oscar Vela', 'Angel Celis', 'Diego González', 'Angel Salinas', 'Pablo García', 'Jaime Farias', 'Roberto Casanova', 'Luis Franco', 'Miguel Camacho'];
 	
 	/* Close Friends */
-	let nombresCloseFriends = ['CloseFriends 1', 'CloseFriends 2', 'CloseFriends 3', 'CloseFriends 4', 'CloseFriends 5', 'CloseFriends 6', 'CloseFriends 7', 'CloseFriends 8', 'CloseFriends 9', 'CloseFriends 10', 'CloseFriends 11'];
+	let nombresCloseFriends = ['Gene 97-98', 'CloseFriends 1', 'CloseFriends 2', 'CloseFriends 3', 'CloseFriends 4', 'CloseFriends 5', 'CloseFriends 6', 'CloseFriends 7', 'CloseFriends 8', 'CloseFriends 9', 'CloseFriends 10', 'CloseFriends 11'];
 	
 	/* Vikingos */
-	let nombresVikingos = ['Mauricio Gutiérrez', 'Juan Pablo Leal', 'Marcelo Guerra', 'Mario Sepúlveda', 'Joel Sepúlveda', 'Emilio Sa', 'Rodrigo Padilla', 'Eugenio Barrera', 'Adrián Ortiz', 'Abelardo Treviño'];
+	let nombresVikingos = ['Gene ??', 'Mauricio Gutiérrez', 'Juan Pablo Leal', 'Marcelo Guerra', 'Mario Sepúlveda', 'Joel Sepúlveda', 'Emilio Sa', 'Rodrigo Padilla', 'Eugenio Barrera', 'Adrián Ortiz', 'Abelardo Treviño'];
 	
 	/* Stacy */
-	let nombresStacy = ['Aléxis Martínez', 'Andrés Treviño', 'David Jaspeado', 'Gerardo Jasso', 'Javier Solís', 'Adrián Martínez', 'Oscar Guardado', 'Juan Pablo Chávez', 'Juan Pablo Quijano', 'Miguel Quintana', 'Emiliano', 'Bernardo Giasi', 'Mauricio Quintana'];
+	let nombresStacy = ['Gene 02', 'Aléxis Martínez', 'Andrés Treviño', 'David Jaspeado', 'Gerardo Jasso', 'Javier Solís', 'Adrián Martínez', 'Oscar Guardado', 'Juan Pablo Chávez', 'Juan Pablo Quijano', 'Miguel Quintana', 'Emiliano', 'Bernardo Giasi', 'Mauricio Quintana'];
 	
 	/* Leyendas */
-	let nombresLeyendas = ['Miguel Vessi', 'Rodrigo García', 'Salvador Valdés', 'José Luis Pier', 'José Ramón Poo', 'Rodrigo Montelongo', 'Marcelo Villarreal', 'Alvaro Llaguno', 'Rodolfo Montelongo', 'Mauricio Sepúlveda', 'Aldo Tamez', 'Mauricio Terab', 'Juan Pablo Rentería'];
+	let nombresLeyendas = ['Gene 00', 'Miguel Vessi', 'Rodrigo García', 'Salvador Valdés', 'José Luis Pier', 'José Ramón Poo', 'Rodrigo Montelongo', 'Marcelo Villarreal', 'Alvaro Llaguno', 'Rodolfo Montelongo', 'Mauricio Sepúlveda', 'Aldo Tamez', 'Mauricio Terab', 'Juan Pablo Rentería'];
 	
 	/* Cerounos */
-	let nombresCerounos = ['Ricardo Pío', 'Roberto Zermeño', 'Daniel Pérez', 'Paco Guerra', 'Juan Pablo Del Angel', 'Diego Treviño', 'Diego Torres Angulo', 'Bernardo Peña', 'Andrés Melendez'];
+	let nombresCerounos = ['Gene 01', 'Ricardo Pío', 'Roberto Zermeño', 'Daniel Pérez', 'Paco Guerra', 'Juan Pablo Del Angel', 'Diego Treviño', 'Diego Torres Angulo', 'Bernardo Peña', 'Andrés Melendez'];
 	
 	/* LaRaza */
-	let nombresLaRaza = ['Juergen Hugler', 'Jorge Tamez', 'Alejandro Meza', 'Carlos Treviño', 'Jorge Castañeda', 'Alejandro Torres', 'Marcelo Colorado', 'Emilio Sánchez', 'Humberto Llovera Cantú', 'Marcelo Garza', 'Daniel Guerra', 'César Quiróga', 'Raúl Treviño', 'Rubén Méndez', 'Manuel García', 'Eduardo Montalvo'];
+	let nombresLaRaza = ['Gene ??', 'Juergen Hugler', 'Jorge Tamez', 'Alejandro Meza', 'Carlos Treviño', 'Jorge Castañeda', 'Alejandro Torres', 'Marcelo Colorado', 'Emilio Sánchez', 'Humberto Llovera Cantú', 'Marcelo Garza', 'Daniel Guerra', 'César Quiróga', 'Raúl Treviño', 'Rubén Méndez', 'Manuel García', 'Eduardo Montalvo'];
 	
 	/* Leones */
-	let nombresLeones = ['Jaime Guerra', 'Adrían Cantú', 'Adrián Estrada', 'Lorenzo Fernández', 'Luis López', 'Sergio Martínez', 'Alan Andonie', 'Luis Lozano', 'Alan De Luna', 'Juan Jose de Isla', 'Marco Rodríguez', 'Adrián Morales', 'Alejandro Guerrero', 'Cristian Najera'];
+	let nombresLeones = ['Gene ??', 'Jaime Guerra', 'Adrían Cantú', 'Adrián Estrada', 'Lorenzo Fernández', 'Luis López', 'Sergio Martínez', 'Alan Andonie', 'Luis Lozano', 'Alan De Luna', 'Juan Jose de Isla', 'Marco Rodríguez', 'Adrián Morales', 'Alejandro Guerrero', 'Cristian Najera'];
 	
 	/* AtleticoSanPancho */
-	let nombresAtleticoSanPancho = ['Diego Gutiérrez', 'Edson Hugler', 'Alan Canales', 'Marcelo Llaguno', 'Erick Villaseñor', 'Andres Miller', 'Fernando García', 'Daniel De la Garza', 'Javier Ceballos', 'Luis Andrés Soto', 'Eugenio Leal', 'Ulises Salinas'];
+	let nombresAtleticoSanPancho = ['Gene ??', 'Diego Gutiérrez', 'Edson Hugler', 'Alan Canales', 'Marcelo Llaguno', 'Erick Villaseñor', 'Andres Miller', 'Fernando García', 'Daniel De la Garza', 'Javier Ceballos', 'Luis Andrés Soto', 'Eugenio Leal', 'Ulises Salinas'];
 	
 	/* Galacticos */
-	let nombresGalacticos = ['Alejandro Minera', 'David Garza', 'Alejandro Álvarez', 'Pedro Vázquez', 'Emilio Ramírez', 'Patricio Alanis', 'Tomás Martínez'];
+	let nombresGalacticos = ['Gene ??', 'Alejandro Minera', 'David Garza', 'Alejandro Álvarez', 'Pedro Vázquez', 'Emilio Ramírez', 'Patricio Alanis', 'Tomás Martínez'];
 	
 	/* Golosos */
-	let nombresGolosos = ['Pablo García', 'Carlos Gutiérrez', 'Patricio Calvillo', 'Gabriel Coronado', 'Santiago Durán', 'Adrián Martínez', 'Adrián Treviño', 'Andres Dillon', 'Javier González', 'Carlos Flores'];
+	let nombresGolosos = ['Gene ??', 'Pablo García', 'Carlos Gutiérrez', 'Patricio Calvillo', 'Gabriel Coronado', 'Santiago Durán', 'Adrián Martínez', 'Adrián Treviño', 'Andres Dillon', 'Javier González', 'Carlos Flores'];
 	
 	/* ElEquipe */
-	let nombresElEquipe = ['Alberto Herrera', 'Diego Pico', 'Moris Camacho', 'Diego Santos', 'Humberto De Leon', 'Josu Monroy', 'Mario Fernández', 'José Monroy', 'Andres De la Garza', 'Alberto Riojas'];
+	let nombresElEquipe = ['Gene ??', 'Alberto Herrera', 'Diego Pico', 'Moris Camacho', 'Diego Santos', 'Humberto De Leon', 'Josu Monroy', 'Mario Fernández', 'José Monroy', 'Andres De la Garza', 'Alberto Riojas'];
 	
 	
 	/* Obtener listas del html */
@@ -124,19 +124,42 @@ function mostrarNombres(numTarjeta, IDlista1, IDlista2, IDcamisa1, IDcamisa2) {
 				break;
 		}
 	
+	var gene = false;
+	
 	dataEquipo1.forEach((item)=>{
+		let small = document.createElement("small");
 		let li = document.createElement("li");
-		li.className = 'list-group-item';
-		li.innerText = item;
-		lista1.appendChild(li);
+		if (!gene) {
+			li.className = 'list-group-item text-center';
+			let strong = document.createElement("strong");
+			lista1.appendChild(li).appendChild(small).appendChild(strong);
+			strong.innerText = item;
+			gene = true;
+		} else {
+			li.className = 'list-group-item';
+			small.innerText = item;
+			lista1.appendChild(li).appendChild(small);
+		}
 	})
 	
+	gene = false;
+	
 	dataEquipo2.forEach((item)=>{
+		let small = document.createElement("small");
 		let li = document.createElement("li");
-		li.className = 'list-group-item';
-		li.innerText = item;
-		lista2.appendChild(li);
-	}) 
+		if (!gene) {
+			li.className = 'list-group-item text-center';
+			let strong = document.createElement("strong");
+			lista2.appendChild(li).appendChild(small).appendChild(strong);
+			strong.innerText = item;
+			gene = true;
+		} else {
+			li.className = 'list-group-item';
+			small.innerText = item;
+			lista2.appendChild(li).appendChild(small);
+		}
+	})
+	
 	
 	card[numTarjeta-1] = true;
 	
